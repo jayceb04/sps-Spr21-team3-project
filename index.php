6ff43f2a4f384b860f55aa4b6b2bc48c386854b2
Andrew Clark <clarkandrew10@gmail.com>
2021-03-27 06:29:56 +0000

    Add Signin page - Changed pages to .php - Adjusted index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php include("includes/header.html");?>
+<!-- this is so we dont have to copy and past the head tags, they will all link to the header.html file -->
+  <body class="bg landing-banner">
+    <section>
+      <div class="mask" style="background-color: rgba(0,0,0,0.6)">
+        <div class="row">
+          <div class="offset-1 col-10 ">
+            <nav class="navbar-expand-lg">
+          <!-- Container wrapper -->
+          <div class="container-fluid">
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse justify-content-center ">
+                <ul class="navbar-nav ms-auto mb-2 landing-nav-center">
+                  <li class="nav-item mr-5 pt-4 ">
+                    <a class="nav-link active" aria-current="page" href="#">[Text Here]</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="/#"><img src="img/node-tree.svg" width=90 height=90 alt="Node Tree"></a>
+                  </li>
+                  <li class="nav-item ml-5 pt-4">
+                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav ml-auto mr-5">
+                  <li class="nav-item pr-5">
+                   <a class="nav-link" href="/signin.php">Sign In</a></li>
+                   <li class="nav-item">
+                     <button class="nav-link outline-btn btn-rounded" href="#">Sign Up</button>
+                 </li>
+               </ul>
+           </div>
+                    <!-- Need to implement the collapsing nav bar -->
+              </div>
+            </div>
+
+
+          </div>
+          <!-- Container wrapper -->
+        </nav>
+        <div class="col-xl-12">
+    			<div class="row">
+    				<div class="col-xl-11 offset-xl-1 landing-copy">
+    					<div class="row">
+    						<div class="col-lg-4">
+                  <div class="row">
+                    <div class="col-2">
+                      <hr>
+                    </div>
+                    <div class="col-10 welcome">
+                      <h2><em>Welcome</em></h1>
+                    </div>
+                  </div>
+                  <ul class="list-header">
+                    <li><h2>Want to study computer Science?</h2></li>
+                    <li><h2>Feel lost in the sea of applications?</h2></li>
+                    <li><h2>Don’t know your next steps?</h2></li>
+                  </ul>
+                  <div class="sub-header">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed euismod nisi porta lorem mollis aliquam ut porttitor leo. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. </p>
+                  </div>
+                  <div class="mt-4">
+                    <button type="button" class="nav-link solid-btn btn-rounded" href="#">Sign Up</button>
+                  </div>
+    						</div>
+                  <div class="col-lg-5 offset-3">
+                    <div class="row">
+                      <div class="col-2">
+                        <hr>
+                      </div>
+                      <div class="col-10 welcome">
+                        <h2><em>Articles</em></h1>
+                      </div>
+                    </div>
+                    <ul class="list-header sub-header">
+                      <li><a href="#">
+                        <h2><em>Lorem Ipsum</em></h2>
+                        <p>John Adams</p>
+                        <p>January 01, 1990</p>
+                      </li></a>
+                      <li><a href="#">
+                        <h2><em>Lorem Ipsum</em></h2>
+                        <p>John Adams</p>
+                        <p>January 01, 1990</p>
+                      </a></li>
+                      <li><a href="#">
+                        <h2><em>Lorem Ipsum</em></h2>
+                        <p>John Adams</p>
+                        <p>January 01, 1990</p>
+                      </a></li>
+                    </ul>
+      						</div>
+                  </div>
+    						</div>
+    					</div>
+    				</div>
+            <div class="gradient">
+          </div>
+        </div>
+      </div>
+
+		</div>
+    <!-- Mask -->
+	</section>
+
+  </body>
+</html>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"></script>
